a7ed1d66f32d764991a75583650a2363f7d06d2c
isaac butchard <59832706+ibcode15@users.noreply.github.com>
2021-06-15 11:36:32 +0100

    Update multsplit.py

--- multsplit.py
+++ multsplit.py
@@ -1,14 +1,21 @@
-def multsplit(str, items_of_split):
+from uuid import uuid4
+
+def Multsplit(string: str, items_of_split: list, Return: type = list, Mapping: dict = {}):
     if len(items_of_split) == 1:
-        return "need more than one item for split"
+        return "need more than one item for splitting"
     elif len(items_of_split) == 0:
-        return "list is empty"
+        return "items_of_split is empty"
     else:
-        try:
-            for x in range(len(items_of_split)):
-                str = str.replace(items_of_split[x],'œ')
-            str = str.split('œ')
-            return str
-        except Exception as e:
-            print(str(e))
-        
+        replace = str(uuid4()) + str(uuid4()) + str(uuid4()) + str(uuid4())
+        for x in range(len(items_of_split)):
+            if items_of_split[x] in Mapping.keys():
+                string = string.replace(items_of_split[x],Mapping[items_of_split[x]])
+            else:
+                string = string.replace(items_of_split[x],replace)
+        string = string.split(replace)
+        if Return == list:
+            return string
+        elif Return == str:
+            return "".join(string)
+        else:
+            return "cannot find type"
